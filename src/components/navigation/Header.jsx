8fb8342b0ada4aa78fadd7201fbd30d823fa1b03
Ильдар Суфияров <ildarmyname@gmail.com>
2025-02-09 13:15:45 +0500

    add adaptation

--- src/components/navigation/Header.jsx
+++ src/components/navigation/Header.jsx
@@ -1,44 +1,122 @@
 import { Link } from "react-router-dom";
-import basketIcon from '../images/basket.png';
-import contactsIcon from '../images/contacts.png';
-import profileIcon from '../images/profile.png';
-import loginIcon from '../images/login.png';
-import informationIcon from '../images/information.png';
+import basketIcon from "../images/basket.png";
+import contactsIcon from "../images/contacts.png";
+import profileIcon from "../images/profile.png";
+import loginIcon from "../images/login.png";
+import informationIcon from "../images/information.png";
+import { useEffect, useState } from "react";
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
     return (
         <header className="header">
-            <nav>
-                <div className="links">
-                    <Link to="/home" className="link logo-link">
-                        <img
-                            src="https://www.amata.kz/local/templates/ilab_it_shop/ilab/img/svg/logo_new.svg"
-                            alt="logo"
-                            title="home"
-                            className="logotype"
-                        />
-                    </Link>
-                    <Link to="/basket" className="link" data-title="Корзина">
-                        <img src={basketIcon} alt="" />
-                    </Link>
-                    <Link to="/about" className="link" data-title="О нас">
-                        <img src={informationIcon} alt="" />
-                    </Link>
-                    <Link to="/contacts" className="link" data-title="Контакты">
-                        <img src={contactsIcon} alt="" />
-                    </Link>
-                    <Link to="/account" className="link" data-title="Личный кабинет">
-                        <img src={profileIcon} alt="" />
-                    </Link>
-                    <Link to="/login" className="link" data-title="Войти">
-                        <img src={loginIcon} alt="" />
-                    </Link>
-                    <Link to="/catalog" className="link">
-                        Каталог товаров
-                    </Link>
-                </div>
-            </nav>
+            <div className="header-desktop">
+                <nav>
+                    <div className="links">
+                        <Link to="/home" className="link logo-link">
+                            <img
+                                src="https://www.amata.kz/local/templates/ilab_it_shop/ilab/img/svg/logo_new.svg"
+                                alt="logo"
+                                title="home"
+                                className="logotype"
+                            />
+                        </Link>
+                        <Link
+                            to="/basket"
+                            className="link"
+                            data-title="Корзина"
+                        >
+                            <img src={basketIcon} alt="" />
+                        </Link>
+                        <Link to="/about" className="link" data-title="О нас">
+                            <img src={informationIcon} alt="" />
+                        </Link>
+                        <Link
+                            to="/contacts"
+                            className="link"
+                            data-title="Контакты"
+                        >
+                            <img src={contactsIcon} alt="" />
+                        </Link>
+                        <Link
+                            to="/account"
+                            className="link"
+                            data-title="Личный кабинет"
+                        >
+                            <img src={profileIcon} alt="" />
+                        </Link>
+                        <Link to="/login" className="link" data-title="Войти">
+                            <img src={loginIcon} alt="" />
+                        </Link>
+                        <Link to="/catalog" className="link">
+                            Каталог товаров
+                        </Link>
+                    </div>
+                </nav>
+            </div>
+            <div className="header-mobile">
+                <nav>
+                    <div className="links">
+                        <Link to="/home" className="link logo-link">
+                            <img
+                                src="https://www.amata.kz/local/templates/ilab_it_shop/ilab/img/svg/logo_new.svg"
+                                alt="logo"
+                                title="home"
+                                className="logotype"
+                            />
+                        </Link>
+                        <Link to="/catalog" className="link">
+                            Каталог товаров
+                        </Link>
+                        <i className="fa-solid fa-bars menu" onClick={toggleMenu}></i>
+                        <div className={`hidden ${isMenuOpen ? 'show' : ''}`}>
+                            <Link
+                                to="/basket"
+                                className="link hidden_link"
+                                data-title="Корзина"
+                            >
+                                <img src={basketIcon} alt="" /> <span className="link_span">Корзина</span>
+                            </Link>
+                            <Link
+                                to="/about"
+                                className="link hidden_link"
+                                data-title="О нас"
+                            >
+                                <img src={informationIcon} alt="" />
+                                <span className="link_span">О нас</span>
+                            </Link>
+                            <Link
+                                to="/contacts"
+                                className="link hidden_link"
+                                data-title="Контакты"
+                            >
+                                <img src={contactsIcon} alt="" />
+                                <span className="link_span">Контакты</span>
+                            </Link>
+                            <Link
+                                to="/account"
+                                className="link hidden_link"
+                                data-title="Личный кабинет"
+                            >
+                                <img src={profileIcon} alt="" />
+                                <span className="link_span">Личный кабинет</span>
+                            </Link>
+                            <Link
+                                to="/login"
+                                className="link hidden_link"
+                                data-title="Войти"
+                            >
+                                <img src={loginIcon} alt="" />
+                                <span className="link_span">Войти</span>
+                            </Link>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </header>
     );
 }
--- src/components/other/catalog/Filters.jsx
+++ src/components/other/catalog/Filters.jsx
@@ -1,99 +1,128 @@
 import data from "./spisok.json";
+import { useState, useEffect } from "react";
 
 function Filters() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    useEffect(() => {
+        const btn = document.querySelector('.filter_title');
+        if (isMenuOpen) {
+            if (btn) {
+                btn.dataset.content = '▲';
+            }
+        } else {
+            if (btn) {
+                btn.dataset.content = "▼"
+            }
+        }
+    })
+    
     return (
-        <div classNameName="filters">
+        <div className="filters">
             <aside className="filter-menu">
-                <h2>Фильтры</h2>
-
-                <div className="filter-group">
-                    <h3>Категории</h3>
-                    <ul>
-                    {data.map((item, index) => (
-                            <li className="item_category" key={index}>
-                                <a href="#" className="link_category">{item.category}</a>
-                            </li>
-                    ))}
-                    </ul>
-                </div>
-
-                <div className="filter-group">
-                    <h3>Бренды</h3>
-                    <ul>
-                        {data.map((item,index) => (
-                            <li className="item_brand" key={index}>
-                            <a className="link_brand" href="#">{item.brand}</a>
-                        </li>
-                        ))}
-                    </ul>
-                </div>
-
-                <div className="filter-group">
-                    <h3>Цена</h3>
-                    <div className="price-range">
-                        <label for="min-price">От:</label>
-                        <input
-                            type="number"
-                            id="min-price"
-                            min="0"
-                            max="1000000000"
-                        />
-                        <label for="max-price">До:</label>
-                        <input
-                            type="number"
-                            id="max-price"
-                            min="0"
-                            max="1000000000"
-                        />
+                <h2 className="filter_title" data-content="▼" onClick={toggleMenu}>
+                    Фильтры
+                </h2>
+                <div className={`hidden_filter ${isMenuOpen ? 'show' : ''}`}>
+                    <div className="filter-group">
+                        <h3>Категории</h3>
+                        <ul>
+                            {data.map((item, index) => (
+                                <li className="item_category" key={index}>
+                                    <a href="#" className="link_category">
+                                        {item.category}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                </div>
 
-                <div className="filter-group">
-                    <h3>Рейтинг</h3>
-                    <div className="rating">
-                        <input
-                            type="radio"
-                            name="rating"
-                            id="rating-4"
-                            value="4"
-                        />
-                        <label for="rating-4">4 звезды и выше</label>
-                        <br />
-                        <input
-                            type="radio"
-                            name="rating"
-                            id="rating-3"
-                            value="3"
-                        />
-                        <label for="rating-3">3 звезды и выше</label>
-                        <br />
-                        <input
-                            type="radio"
-                            name="rating"
-                            id="rating-2"
-                            value="2"
-                        />
-                        <label for="rating-2">2 звезды и выше</label>
-                        <br />
-                        <input
-                            type="radio"
-                            name="rating"
-                            id="rating-1"
-                            value="1"
-                        />
-                        <label for="rating-1">1 звезда и выше</label>
+                    <div className="filter-group">
+                        <h3>Бренды</h3>
+                        <ul>
+                            {data.map((item, index) => (
+                                <li className="item_brand" key={index}>
+                                    <a className="link_brand" href="#">
+                                        {item.brand}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                </div>
 
-                <div className="filter-group">
-                    <h3>В наличии</h3>
-                    <div className="availability">
-                        <input type="checkbox" id="in-stock" />
-                        <label for="in-stock">Только в наличии</label>
+                    <div className="filter-group">
+                        <h3>Цена</h3>
+                        <div className="price-range">
+                            <label for="min-price">От:</label>
+                            <input
+                                type="number"
+                                id="min-price"
+                                min="0"
+                                max="1000000000"
+                            />
+                            <label for="max-price">До:</label>
+                            <input
+                                type="number"
+                                id="max-price"
+                                min="0"
+                                max="1000000000"
+                            />
+                        </div>
                     </div>
-                </div>
 
-                <button type="submit">Применить</button>
+                    <div className="filter-group">
+                        <h3>Рейтинг</h3>
+                        <div className="rating">
+                            <input
+                                type="radio"
+                                name="rating"
+                                id="rating-4"
+                                value="4"
+                            />
+                            <label for="rating-4">4 звезды и выше</label>
+                            <br />
+                            <input
+                                type="radio"
+                                name="rating"
+                                id="rating-3"
+                                value="3"
+                            />
+                            <label for="rating-3">3 звезды и выше</label>
+                            <br />
+                            <input
+                                type="radio"
+                                name="rating"
+                                id="rating-2"
+                                value="2"
+                            />
+                            <label for="rating-2">2 звезды и выше</label>
+                            <br />
+                            <input
+                                type="radio"
+                                name="rating"
+                                id="rating-1"
+                                value="1"
+                            />
+                            <label for="rating-1">1 звезда и выше</label>
+                        </div>
+                    </div>
+
+                    <div className="filter-group">
+                        <h3>В наличии</h3>
+                        <div className="availability">
+                            <input type="checkbox" id="in-stock" />
+                            <label for="in-stock">Только в наличии</label>
+                        </div>
+                    </div>
+
+                    <button className="style_btn" type="submit">
+                        Применить
+                    </button>
+                </div>
             </aside>
         </div>
     );
